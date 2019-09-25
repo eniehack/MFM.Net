e3623920cfb985b54107fc1e4d7570add2ccaf5f
caffeine <eniehack@outlook.jp>
2019-09-26 01:09:54 +0900

    簡易的なテストを書いた(動作せず)

--- Program.fs
+++ Program.fs
@@ -1,8 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
+open System.IO
+open FSharp.Text.Lexing
+
+let LexAndParseFromString (text : string) =
+    let lexbuf = LexBuffer<char>.FromString text
+    let countFromParser = Parser.start Lexer.tokenize lexbuf
+
+    printfn "%O" countFromParser
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    LexAndParseFromString "**A**"
+    //LexAndParseFromString "text"
     0 // return an integer exit code
